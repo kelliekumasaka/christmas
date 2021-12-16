0b581796d238961010ba7e6b769ff72cdd986d27
kelliekumasaka <kelliek3@gmail.com>
2021-12-15 22:40:52 -0800

    import uikit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import "../node_modules/uikit/dist/js/uikit.min.js";
+import "../node_modules/uikit/dist/css/uikit.min.css"
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
 
+UIkit.use(Icons);
 
 ReactDOM.render(
   <React.StrictMode>
