9d7e4c142633765c845ce53873950b3baab81320
kelliekumasaka <kelliek3@gmail.com>
2021-12-15 21:33:24 -0800

    final letter

---
+++ src/components/Letter/Letter.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+function Letter (){
+    return(
+        <>
+            <p>
+                Welcome to first ever Kumasaka *online* newsletter!! Built by yours truly, the Kumasaka's are officially welcoming in the online era. I hope it wasn't too hard to scan the QR code or type in the website, but from now on, our newsletters will be hosted on this site so be sure to write it down, bookmark it, check this website as "Important"! Moving on to our actual yearly updates, 2021 has been a year of constant adjustment and seemingly endless new regulations, but despite that, we were able to go to Hawaii and Portland a couple of times for family trips. Now let's see what the individual members of the family were up to!
+            </p>
+            <p>Dubbie:</p>
+            <p>
+                This year was another rough year for Seattle sports, but in the midst of having his heartbroken by the Mariners yet again, he became a hockey fan and greatly enjoys going to the new indoor arena. Dubbie is, much to his own disappointment, not retired and still at Valley... although he's at Bellevue Municipal he basically has a part time job there (shout out to those that are willing to "walk the dog"... You know who you are). Dad has also decided to continue as an elder at Presby and is proud to say that we were the last church to start meeting in person... weird flex.
+            </p>
+            <p>Mubbie:</p>
+            <p>
+                Mubbie was quite busy this year moving her two daughters into two different cities, golfing with the ladies, and hosting the occassional lunch in our transformed below-deck area, "the den". She also turned 60 and can officially make use of all the senior discounts, which is actually quite exciting. During quarantine, Mubbie has decided to take up crocheting and sewing again, so look forward to some hand-crocheted bucket hats soon.
+            </p>
+            <p>Kellie:</p>
+            <p>
+                Yours truly. 2021 has been a year of funemployment, cheap Uwajimaya food, a brief stint in sales, and finally a complete 180 to a web development boot camp. Yes, it's been quite a year. I've also had some fun times playing softball, traveling to Leavenworth, and picking up a new old hobby: Pokemon card collecting. I'm currently still living at home, to the joy of my parents, while searching for work.
+            </p>
+            <p>Lisa:</p>
+            <p>
+                This year, Lisa has had quite an eventful year. Even in the midst of the ever-changing pandemic situation, Lisa managed to make her way to Montenegro-Croatia-Greece, then to Chicago, and finally, to New York. Oh yeah, and she started med school at Oregon Health & Science University, is a lead for the healthcare equity fair, and a co-lead for the Christian Medical and Dental Association... Work hard play hard I guess? 
+            </p>
+            <p>Jill:</p>
+            <p>
+                Is our current ORISE Fellow, working with (not for, very important distinction) the CDC. She moved back home during the summer after successfully killing her junior year, taking the MCAT, and graduated early from UW, as the last of the undergrads in our family to do so. Amidst tutoring, hanging out with friends, studying and ORISE Fellow-ing, she even sometimes makes time for her family... if we make an appointment in her GoogleCalendar, that is. Jill is looking forward to moving to Georgia to continue working with the CDC and is excited "to get away from you fools".
+            </p>
+            <p>Luna:</p>
+            <p>
+                Our skinny little man of a dog is just as energetic as ever, even with a very short haircut. She has also realized that her favorite person is none other than me and can almost always be found lying on my bed. Luna hopes to spend more time with her friends Kai, Rem, and Kona this upcoming year.
+            </p>
+        </>
+    )
+}
+
+export default Letter;
