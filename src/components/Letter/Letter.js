b8656f0c779274e59449cfffb39b8d036ab799e6
Kellie Kumasaka <kellie.kumasaka@logixboard.com>
2022-12-19 20:35:20 -0800

    more snow

--- src/components/Letter/Letter.js
+++ src/components/Letter/Letter.js
@@ -1,67 +1,92 @@
 import React from "react";
-import "./Letter.css"
+import "./Letter.css";
 
-function Letter (){
-    return(
-        <div id="hello">
-            <p>
-            2020 and 2021 brought several things: the COVID pandemic, social distancing, and two incredibly forgettable
-            Kumasaka newsletters. But after two long years of waiting, I’M BACK BABY. That’s right folks, your author this
-            year is Jill. In 2022, my family embraced technology to its fullest with solar panels for the house, a new fully
-            electric car, and Fubo TV. We also acquired a younger Japanese brother, which, unlike everything else in this
-            letter, is not a joke! Hanamichi, our cousin from Japan, started living with us in July and has been a great
-            addition to the chaos of our household.
-            </p>
-            <h3>DUBBIE:</h3>
-            <p>
-                My father turned 60 this year and celebrated with the “Bandon Boys” which is basically a bad middle-
-                aged boyband that golfs and drinks whiskey. Dubbie also went flyfishing for the first time in 40 years with his
-                favorite brother which he describes as, “truly one of the biggest thrills this year.” Oh, to be old and enjoying
-                “thrilling” activities like golfing and fly fishing. He is enjoying his transition to part-time dermatologist at Valley
-                Medical (as if he ever worked in the first place), golfing, and cheering for Seattle sports teams.
-            </p>
-            <h3>MUBBIE:</h3>
-            <p>
-                Mother is as active as ever. She enjoys teaching her Sunday school lessons featuring embarrassing
-                stories of her daughters and serving as a deacon. Throughout this year, you could find Mubbie either cheering
-                for the Mariners in the living room or golfing with Auntie Wendy or Julie or Leslie or Marji or all the above. Her
-                2022 highlights include taking selfies with Stormtroopers on Rise of the Resistance, watching kdrama, and
-                spending time with her favorite daughter (not to be confused with the other daughter who lives with her, Kellie).
-            </p>
-            <h3>KIWI:</h3>
-            <p>
-                Everyone’s least favorite sister had an eventful year: she saw Twice twice, caught COVID in Toronto,
-                and got a job, got a job, quit a job, and got a job (if you’re confused by that last part then that makes two of us).
-                She is now working from home as a software engineer for a shipping tracking company called Logixboard.
-                Kellie often complains that her one and only in-office coworker smells bad and makes too much noise, but
-                Luna claims otherwise. Kiwi is also tutoring students in the computer science boot camp she completed last
-                year. Overall, she stays busy buying Pokémon cards, bullet journaling, and hanging out with friends.
-            </p>
-            <h3>TOW TOW:</h3>
-            <p>
-                This year, Lisa has had quite an eventful year. Even in the midst of the ever-changing pandemic situation, Lisa managed to make her way to Montenegro-Croatia-Greece, then to Chicago, and finally, to New York. Oh yeah, and she started med school at Oregon Health & Science University, is a lead for the healthcare equity fair, and a co-lead for the Christian Medical and Dental Association... Work hard play hard I guess? 
-            </p>
-            <h3>JEEL:</h3>
-            <p>
-                Onto the important stuff. This year I saw Twice once, travelled across the country with my father (and
-                survived!), and skydived in Hawaii. In January, I moved to Atlanta for six months to work for the CDC. While in
-                Atlanta, I also started working as a barista where I “developed a passion for coffee” which my family interprets
-                as “became a coffee snob.” I am currently pursuing an original and very foreign profession to our family
-                (medicine) and working as a medical scribe for the best doctor in Des Moines (ily Dr. Okamoto).
-            </p>
-            <h3>LUNA:</h3>
-            <p>
-                Luna is… the same: fuzzy, lazy, and stinky. Her hobbies of 2022 include harassing children, barking at cats, and scratching herself.
-            </p>
-            <p>
-                Well, that’s a wrap people! If you enjoyed this year’s newsletter, please kindly remind my supervisors to give their workers a raise. Thanks again for tuning in and happy holidays!
-            </p>
-            <p>
-                With love,
-                The Kumasakas
-            </p>
-        </div>
-    )
+function Letter() {
+  return (
+    <div id="hello">
+      <p>
+        2021 brought several things: more COVID pandemic, more social
+        distancing, and one incredibly forgettable Kumasaka newsletters. But
+        after one long year of waiting, I’M BACK BABY. That’s right folks, your
+        author this year, brought back one year sooner than usual by popular
+        demand (and because my parents forgot -_-) is Jill. In 2022, my family
+        embraced technology to its fullest with solar panels for the house, a
+        new fully electric car, and Fubo TV. We also acquired a younger Japanese
+        brother, which, unlike everything else in this letter, is not a joke!
+        Hanamichi, our cousin from Japan, started living with us in July and has
+        been a great addition to the chaos of our household.
+      </p>
+      <h3>DUBBIE:</h3>
+      <p>
+        My father turned 60 this year and celebrated with the “Bandon Boys”
+        which is basically a bad middle- aged boyband that golfs and drinks
+        whiskey. Dubbie also went flyfishing for the first time in 40 years with
+        his favorite brother which he describes as, “truly one of the biggest
+        thrills this year.” Oh, to be old and enjoying “thrilling” activities
+        like golfing and fly fishing. He is enjoying his transition to part-time
+        dermatologist at Valley Medical (as if he ever worked in the first
+        place), golfing, and cheering for Seattle sports teams.
+      </p>
+      <h3>MUBBIE:</h3>
+      <p>
+        Mother is as active as ever. She enjoys teaching her Sunday school
+        lessons featuring embarrassing stories of her daughters and serving as a
+        deacon. Throughout this year, you could find Mubbie either cheering for
+        the Mariners in the living room or golfing with Auntie Wendy or Julie or
+        Leslie or Marji or all the above. Her 2022 highlights include taking
+        selfies with Stormtroopers on Rise of the Resistance, watching kdrama,
+        and spending time with her favorite daughter (not to be confused with
+        the other daughter who lives with her, Kellie).
+      </p>
+      <h3>KIWI:</h3>
+      <p>
+        Everyone’s least favorite sister had an eventful year: she saw Twice
+        twice, caught COVID in Toronto, and got a job, got a job, quit a job,
+        and got a job (if you’re confused by that last part then that makes two
+        of us). She is now working from home as a software engineer for a
+        shipping tracking company called Logixboard. Kellie often complains that
+        her one and only in-office coworker smells bad and makes too much noise,
+        but Luna claims otherwise. Kiwi is also tutoring students in the
+        computer science boot camp she completed last year. Overall, she stays
+        busy buying Pokémon cards, bullet journaling, and hanging out with
+        friends.
+      </p>
+      <h3>TOW TOW:</h3>
+      <p>
+        Lisa is pretending to study at OHSU in her second year of Medical
+        School. She is also single- handedly solving medical racism and finding
+        the cure to cancer, or at least that’s what it seems like. Tow Tow is
+        the swollest in the PNW and can squat the equivalent of two Kiwi’s + one
+        Luna, you do the math. This year, she moved to a new house, led OHSU’s
+        Health Care Equity Fair, and stitched up a knife wound! She also
+        fulfilled her vigilante dreams and almost died, but that’s a story for
+        another time.
+      </p>
+      <h3>JEEL:</h3>
+      <p>
+        Onto the important stuff. This year I saw Twice once, travelled across
+        the country with my father (and survived!), and skydived in Hawaii. In
+        January, I moved to Atlanta for six months to work for the CDC. While in
+        Atlanta, I also started working as a barista where I “developed a
+        passion for coffee” which my family interprets as “became a coffee
+        snob.” I am currently pursuing an original and very foreign profession
+        to our family (medicine) and working as a medical scribe for the best
+        doctor in Des Moines (ily Dr. Okamoto).
+      </p>
+      <h3>LUNA:</h3>
+      <p>
+        Luna is… the same: fuzzy, lazy, and stinky. Her hobbies of 2022 included
+        harassing children, barking at cats, and scratching herself.
+      </p>
+      <p>
+        Well, that’s a wrap people! If you enjoyed this year’s newsletter,
+        please kindly remind my supervisors to give their workers a raise.
+        Thanks again for tuning in and happy holidays!
+      </p>
+      <p style={{textAlign: 'center'}}>With love,</p>
+      <p style={{textAlign: 'center'}}>The Kumasakas</p>
+    </div>
+  );
 }
 
 export default Letter;
--- src/index.js
+++ src/index.js
@@ -5,13 +5,21 @@ import "../node_modules/uikit/dist/js/uikit.min.js";
 import "../node_modules/uikit/dist/css/uikit.min.css"
 import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
-import "./index.css"
+import "./index.css";
+import Snowfall from 'react-snowfall';
 
 UIkit.use(Icons);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div style={{ background: '#282c34' }}>
+      <Snowfall snowflakeCount={200} style={{
+        position: 'fixed',
+        width: '100%',
+        height: '100%',
+      }} />
+      <App />
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
