b1bb9753828dac6dd7ee9e9612deff539d364087
Kellie Kumasaka <kellie.kumasaka@logixboard.com>
2022-12-19 21:12:30 -0800

    image

--- src/index.js
+++ src/index.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 import "../node_modules/uikit/dist/js/uikit.min.js";
-import "../node_modules/uikit/dist/css/uikit.min.css"
-import UIkit from 'uikit';
-import Icons from 'uikit/dist/js/uikit-icons';
+import "../node_modules/uikit/dist/css/uikit.min.css";
+import UIkit from "uikit";
+import Icons from "uikit/dist/js/uikit-icons";
 import "./index.css";
-import Snowfall from 'react-snowfall';
+import Snowfall from "react-snowfall";
+import dub3 from "./images/final.png"
+
+const snowflake3 = document.createElement("img");
+snowflake3.src = dub3;
+
+const images = [ snowflake3];
 
 UIkit.use(Icons);
 
 ReactDOM.render(
   <React.StrictMode>
-    <div style={{ background: '#282c34' }}>
-      <Snowfall snowflakeCount={200} style={{
-        position: 'fixed',
-        width: '100%',
-        height: '100%',
-      }} />
+    <div style={{ background: "#282c34" }}>
+      <Snowfall
+        snowflakeCount={200}
+      />
+      <Snowfall
+        snowflakeCount={10}
+        style={{
+          position: "fixed",
+          width: "100%",
+          height: "100%",
+        }}
+        images={images}
+        radius={[20.0, 30.0]}
+      />
       <App />
     </div>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
