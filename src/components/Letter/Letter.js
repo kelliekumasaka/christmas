c03fbd66ee6e56fac48e1723ff8553d03ac55f3e
Kellie Kumasaka <kellie.kumasaka@logixboard.com>
2023-12-12 14:08:53 -0800

    new letter

--- src/components/Letter/Letter.js
+++ src/components/Letter/Letter.js
@@ -5,86 +5,80 @@ function Letter() {
   return (
     <div id="hello">
       <p>
-        2021 brought several things: more COVID pandemic, more social
-        distancing, and one incredibly forgettable Kumasaka newsletters. But
-        after one long year of waiting, I’M BACK BABY. That’s right folks, your
-        author this year, brought back one year sooner than usual by popular
-        demand (and because my parents forgot -_-) is Jill. In 2022, my family
-        embraced technology to its fullest with solar panels for the house, a
-        new fully electric car, and Fubo TV. We also acquired a younger Japanese
-        brother, which, unlike everything else in this letter, is not a joke!
-        Hanamichi, our cousin from Japan, started living with us in July and has
-        been a great addition to the chaos of our household.
+      Well, strap in folks because it has been QUITE the year for the Kumasaka family! Our family had
+      the privilege of visiting London and Paris together (don’t worry this was before the bed bug
+      debacle…). We ate our weight in croissants, spent 8+ hours at the Harry Potter Studio, popped
+      champagne beneath the Eiffel Tower, snapped a selfie with Mona Lisa, and generally made
+      fools of ourselves as typical American tourists! Kellie, Jill and I also spent time traveling to Spain
+      and Portugal! Well, actually, I had to return back to the states before the Portugal leg of the trip
+      as *some* of us were hardworking students during that time with no such thing as “unlimited
+      PTO” and “funemployment”…
       </p>
-      <h3>DUBBIE:</h3>
+      <h3>BHK:</h3>
       <p>
-        My father turned 60 this year and celebrated with the “Bandon Boys”
-        which is basically a bad middle- aged boyband that golfs and drinks
-        whiskey. Dubbie also went flyfishing for the first time in 40 years with
-        his favorite brother which he describes as, “truly one of the biggest
-        thrills this year.” Oh, to be old and enjoying “thrilling” activities
-        like golfing and fly fishing. He is enjoying his transition to part-time
-        dermatologist at Valley Medical (as if he ever worked in the first
-        place), golfing, and cheering for Seattle sports teams.
+      Speaking of funemployment…I’m just kidding our dad is STILL working but if you want to
+      get an appointment with one of Valley’s best dermatologists, it’ll probably be a very long wait
+      as this hardworking doc enjoys his 4-day weekends! Dubbie’s 2023 has been filled with lots of
+      golfing, miraculously realizing golf should be fun, running (including a 5k in Central Park!),
+      getting hurt while running, making a somewhat full recovery, then proceeding to run again,
+      vibing with his bff Bruno Mars in Las Vegas, organizing a family NHL Fantasy League, and of
+      course, going to Kraken, Mariners, and Huskies games!
       </p>
-      <h3>MUBBIE:</h3>
+      <h3>SMK:</h3>
       <p>
-        Mother is as active as ever. She enjoys teaching her Sunday school
-        lessons featuring embarrassing stories of her daughters and serving as a
-        deacon. Throughout this year, you could find Mubbie either cheering for
-        the Mariners in the living room or golfing with Auntie Wendy or Julie or
-        Leslie or Marji or all the above. Her 2022 highlights include taking
-        selfies with Stormtroopers on Rise of the Resistance, watching kdrama,
-        and spending time with her favorite daughter (not to be confused with
-        the other daughter who lives with her, Kellie).
+        Our mother is hard at work in her KDECWMTAKDBMJS (Korean Drama Enthusiasts Club
+        Who Meet and Talk About Korean Dramas But Mostly Just Socialize)! When she’s not picking up
+        my FaceTime calls to ask her if it’s safe to eat my expired food, she’s picking up Jill’s FaceTime
+        calls to listen to her rant about the “hardest test of her life.” She is also keeping busy staying
+        involved with Presby by teaching Sunday School for the cutest 2-3 y/o’s and serving as a
+        deacon. What my mom is most looking forward to in 2024: being an empty nester! I feel like
+        we’ve been saying our parents will be empty nesters in every Christmas letter since 2019…but
+        we’re serious this time.
       </p>
-      <h3>KIWI:</h3>
+      <h3>KHK:</h3>
       <p>
-        Everyone’s least favorite sister had an eventful year: she saw Twice
-        twice, caught COVID in Toronto, and got a job, got a job, quit a job,
-        and got a job (if you’re confused by that last part then that makes two
-        of us). She is now working from home as a software engineer for a
-        shipping tracking company called Logixboard. Kellie often complains that
-        her one and only in-office coworker smells bad and makes too much noise,
-        but Luna claims otherwise. Kiwi is also tutoring students in the
-        computer science boot camp she completed last year. Overall, she stays
-        busy buying Pokémon cards, bullet journaling, and hanging out with
-        friends.
+        This was arguably the most important year for our beloved, Keloid. As you IG followers
+        know, Kellie got engaged this year to the one and only skater boi Nyle Miura, who
+        (unfortunately for him but fortunately for us) will be committing his life to handle the terrifying
+        while hangry bundle of joy that is my older sister! We are eternally grateful Nyle. Kellie was
+        quite the world traveler this year spending time in Mexico, Spain, Portugal, and Japan! She says,
+        and I quote, “Spain was probably the best trip since my dearest sister Lisa was traveling with
+        me.” Bless her soul. I’m not adding anything about her work since I’m not quite sure what her
+        job entails other than sitting in front of a computer from 8am-5pm and making sure her
+        website doesn’t crash?...
       </p>
       <h3>TOW TOW:</h3>
       <p>
-        Lisa is pretending to study at OHSU in her second year of Medical
-        School. She is also single- handedly solving medical racism and finding
-        the cure to cancer, or at least that’s what it seems like. Tow Tow is
-        the swollest in the PNW and can squat the equivalent of two Kiwi’s + one
-        Luna, you do the math. This year, she moved to a new house, led OHSU’s
-        Health Care Equity Fair, and stitched up a knife wound! She also
-        fulfilled her vigilante dreams and almost died, but that’s a story for
-        another time.
+        Well, as for your humble author not a lot has changed…I could pretty much just copy and
+        paste from last year’s letter, but since you all skipped the first half of this letter to jump down
+        to my paragraph, I might as well update you! I am currently in my MCR (Master of Clinical
+        Research) year in between my second and third year of medical school. I’ve spent most of this
+        year in lab running experiments or organizing events like the Asian Pacific American Medical
+        Student Association National Conference! I most recently traveled to Philly to attend the Kidney
+        Week conference! Just imagine 12,000 nerdy, kidney enthusiasts + some cheesesteaks + the Liberty Bell and that just about sums it up! ALSO, I adopted a bb husky in March, Kaia, who has
+        kept me on my toes and has substantially increased my daily average steps.
       </p>
-      <h3>JEEL:</h3>
+      <h3>JKK:</h3>
       <p>
-        Onto the important stuff. This year I saw Twice once, travelled across
-        the country with my father (and survived!), and skydived in Hawaii. In
-        January, I moved to Atlanta for six months to work for the CDC. While in
-        Atlanta, I also started working as a barista where I “developed a
-        passion for coffee” which my family interprets as “became a coffee
-        snob.” I am currently pursuing an original and very foreign profession
-        to our family (medicine) and working as a medical scribe for the best
-        doctor in Des Moines (ily Dr. Okamoto).
+        As for my younger sister, she missed me so much the past 2 years that she decided to
+        follow me to good ol’ P-town and matriculate at Oregon Health &amp; Science University School of
+        Medicine and is now a member of the MD/MPH class of 2028! I would say she is “surviving not
+        thriving”, as I think most med students would describe their usual day-to-day vibes. Before
+        starting at OHSU, Jill was livin’ the good life and by that, I mean the rent free, home-cooked
+        meals good life. She worked as a medical scribe before starting at OHSU and spent a bit time of
+        time traveling to New York along with the other family vacations!
       </p>
-      <h3>LUNA:</h3>
+      <h3>LPWK (Luna Poona Woona Kumasaka):</h3>
       <p>
-        Luna is… the same: fuzzy, lazy, and stinky. Her hobbies of 2022 included
-        harassing children, barking at cats, and scratching herself.
+        Ok, now I can ACTUALLY copy and paste from last year’s
+        letter…Luna, our precious, mustache-toting, socially inept, labradoodle is doing just fine. She
+        enjoys her naps in the sun, being Kellie’s only WFH co-worker, and running!
       </p>
       <p>
-        Well, that’s a wrap people! If you enjoyed this year’s newsletter,
-        please kindly remind my supervisors to give their workers a raise.
-        Thanks again for tuning in and happy holidays!
+        Well, that’s a wrap for 2023! Happy Christmas and Merry New Year!
       </p>
-      <p style={{textAlign: 'center'}}>With love,</p>
-      <p style={{textAlign: 'center'}}>The Kumasakas</p>
+      <p style={{textAlign: 'center'}}>Love,</p>
+      <p style={{textAlign: 'center'}}>The Kumasaka fam</p>
     </div>
   );
 }
