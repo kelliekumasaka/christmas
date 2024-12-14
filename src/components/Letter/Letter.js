c034096726570d308e00f3b08082772219c14dfa
Kellie Kumasaka <kelliekumasaka@Kellies-MacBook-Pro.local>
2024-12-13 17:09:36 -0800

    nyle letter

--- src/components/Letter/Letter.js
+++ src/components/Letter/Letter.js
@@ -1,85 +1,69 @@
-import React from "react";
-import "./Letter.css";
+import React from 'react';
+import './Letter.css';
+// import { useFetchComments } from '../../api/hooks';
 
 function Letter() {
+  // const [name, setName] = useState('')
+  // const [comment, setComment] = useState('')
+  // // use hook to get 
+  // const { currComment } = useFetchComments()
+
+  // const submitHandler = (event) => {
+  //   // call fs to write doc
+  //   event.preventDefault();
+  //   console.log(name, comment);
+    
+  // }
+
   return (
-    <div id="hello">
-      <p>
-      Well, strap in folks because it has been QUITE the year for the Kumasaka family! Our family had
-      the privilege of visiting London and Paris together (don’t worry this was before the bed bug
-      debacle…). We ate our weight in croissants, spent 8+ hours at the Harry Potter Studio, popped
-      champagne beneath the Eiffel Tower, snapped a selfie with Mona Lisa, and generally made
-      fools of ourselves as typical American tourists! Kellie, Jill and I also spent time traveling to Spain
-      and Portugal! Well, actually, I had to return back to the states before the Portugal leg of the trip
-      as *some* of us were hardworking students during that time with no such thing as “unlimited
-      PTO” and “funemployment”…
-      </p>
-      <h3>BHK:</h3>
-      <p>
-      Speaking of funemployment…I’m just kidding our dad is STILL working but if you want to
-      get an appointment with one of Valley’s best dermatologists, it’ll probably be a very long wait
-      as this hardworking doc enjoys his 4-day weekends! Dubbie’s 2023 has been filled with lots of
-      golfing, miraculously realizing golf should be fun, running (including a 5k in Central Park!),
-      getting hurt while running, making a somewhat full recovery, then proceeding to run again,
-      vibing with his bff Bruno Mars in Las Vegas, organizing a family NHL Fantasy League, and of
-      course, going to Kraken, Mariners, and Huskies games!
-      </p>
-      <h3>SMK:</h3>
-      <p>
-        Our mother is hard at work in her KDECWMTAKDBMJS (Korean Drama Enthusiasts Club
-        Who Meet and Talk About Korean Dramas But Mostly Just Socialize)! When she’s not picking up
-        my FaceTime calls to ask her if it’s safe to eat my expired food, she’s picking up Jill’s FaceTime
-        calls to listen to her rant about the “hardest test of her life.” She is also keeping busy staying
-        involved with Presby by teaching Sunday School for the cutest 2-3 y/o’s and serving as a
-        deacon. What my mom is most looking forward to in 2024: being an empty nester! I feel like
-        we’ve been saying our parents will be empty nesters in every Christmas letter since 2019…but
-        we’re serious this time.
-      </p>
-      <h3>KHK:</h3>
-      <p>
-        This was arguably the most important year for our beloved, Keloid. As you IG followers
-        know, Kellie got engaged this year to the one and only skater boi Nyle Miura, who
-        (unfortunately for him but fortunately for us) will be committing his life to handle the terrifying
-        while hangry bundle of joy that is my older sister! We are eternally grateful Nyle. Kellie was
-        quite the world traveler this year spending time in Mexico, Spain, Portugal, and Japan! She says,
-        and I quote, “Spain was probably the best trip since my dearest sister Lisa was traveling with
-        me.” Bless her soul. I’m not adding anything about her work since I’m not quite sure what her
-        job entails other than sitting in front of a computer from 8am-5pm and making sure her
-        website doesn’t crash?...
-      </p>
-      <h3>LTK:</h3>
-      <p>
-        Well, as for your humble author not a lot has changed…I could pretty much just copy and
-        paste from last year’s letter, but since you all skipped the first half of this letter to jump down
-        to my paragraph, I might as well update you! I am currently in my MCR (Master of Clinical
-        Research) year in between my second and third year of medical school. I’ve spent most of this
-        year in lab running experiments or organizing events like the Asian Pacific American Medical
-        Student Association National Conference! I most recently traveled to Philly to attend the Kidney
-        Week conference! Just imagine 12,000 nerdy, kidney enthusiasts + some cheesesteaks + the Liberty Bell and that just about sums it up! ALSO, I adopted a bb husky in March, Kaia, who has
-        kept me on my toes and has substantially increased my daily average steps.
-      </p>
-      <h3>JKK:</h3>
-      <p>
-        As for my younger sister, she missed me so much the past 2 years that she decided to
-        follow me to good ol’ P-town and matriculate at Oregon Health &amp; Science University School of
-        Medicine and is now a member of the MD/MPH class of 2028! I would say she is “surviving not
-        thriving”, as I think most med students would describe their usual day-to-day vibes. Before
-        starting at OHSU, Jill was livin’ the good life and by that, I mean the rent free, home-cooked
-        meals good life. She worked as a medical scribe before starting at OHSU and spent a bit time of
-        time traveling to New York along with the other family vacations!
-      </p>
-      <h3>LPWK (Luna Poona Woona Kumasaka):</h3>
-      <p>
-        Ok, now I can ACTUALLY copy and paste from last year’s
-        letter…Luna, our precious, mustache-toting, socially inept, labradoodle is doing just fine. She
-        enjoys her naps in the sun, being Kellie’s only WFH co-worker, and running!
-      </p>
-      <p>
-        Well, that’s a wrap for 2023! Happy Christmas and Merry New Year!
-      </p>
-      <p style={{textAlign: 'center'}}>Love,</p>
-      <p style={{textAlign: 'center'}}>The Kumasaka fam</p>
-    </div>
+    <>
+      <div id='hello'>
+        <p>
+          Merry Christmas everybody! Like a second string quarterback coming straight off the bench to lead the game winning drive, this Christmas card is brought to you by the one and only <span style={{textDecoration: 'line-through'}}>Geno Smith</span> Nyle Miura! It’s been a hectic but ultimately memorable first year as part of the Kumasaka family. As you can see, they’re already putting me to work (<span style={{textDecoration: 'line-through'}}>help me</span>). But I think it’s the least I can do for welcoming me into their family. 
+          <br/><br/>
+          The biggest event we did together this year was our trip to Korea and Japan! In Korea, we explored Seoul, took some pictures wearing traditional Hanboks, visited some KDrama landmarks, and ate tons. And in Japan, we explored Tokyo/Osaka, visited TeamLab Planets, hit baseballs in a batting cage, shopped, and had delicious omakase. I sometimes hear about how going on trips with a group of friends/family can be very telling about the stability of those relationships. And I’m happy to inform that the trip did not tear our family apart!
+          <br/><br/>
+          Now as for the individuals, Kellie has refused to provide me with any further information. So this is purely based on what I know…
+        </p>
+        <h3>BHK (AKA Dub)</h3>
+        <p>
+        When I think of my father in law, I think of three things: running, golf, and Seattle sports. He recently completed a half marathon in the blazing California heat. Honestly props to that. He’s in better shape than I am! When he’s not running, he’s usually out on the golf course. Not sure if this is an exaggeration but it seemed like he was out of state on a golf trip at least once a month. And once again, we were able to mourn together the decline of the 2024 Mariner’s season where they failed to make the playoffs again. At least it’s Football and Hockey season now (if you want to buy Kraken tickets, reach out to him).
+        </p>
+        <h3>SMK (AKA Mub)</h3>
+        <p>
+        As for my wonderful mother in law, it’s also been a very busy 2024. From her continuation in her K-Drama watch club, to also killing it on the golf course! And not to mention all the holidays and dinners she’s labored for so that her family and loved ones can enjoy a meal together. When I think of her, the word “gratitude” is the first that comes to mind. Since this was her first year as an empty nester, I’m sure it’s been a much quieter year overall. Although, we still come over often for some not-so-friendly games of Shanghai Rummy, which includes a lot of yelling. So maybe it’s not that quiet after all.
+        </p>
+        <h3>LTK</h3>
+        <p>
+        Another year has gone by of Lisa just doing her thing in Portland. Other than picking up lunches for her senior medical staff members, I imagine her day to day is some combination of walking her big dog Kaia (seriously this dog is bigger than Kellie), research, helping save lives, and receiving zero compensation. One of the best stories from this year involved her and I in Korea. Us and Kellie were on our way to the Hanbok photoshoot. When the bus arrived, Kellie proceeded to get on the bus, but Lisa and I were denied entry because we had coffees in hand. There was a slight delay in panic and before we knew it, the doors had closed, Kellie was on the bus alone as it drove away, and Lisa and I were chasing it on the streets of Seoul! It was probably the sweatiest I was all trip, and I don’t think either of us will be forgetting that moment anytime soon.
+        </p>
+        <h3>JKK</h3>
+        <p>
+        Since Jill is following a very similar course to Lisa, I thought maybe I could copy and paste Lisa’s Christmas letter from two years ago here… But in all seriousness, I know she is also continuing to kill it down in Portland. I believe she has some kind of big test coming up in early 2025 that she’s stressing about. But the rest of us have full confidence in her! In our most recent trip to Japan, we spent a lot of time eating, exploring, and shopping together. One funny story from Japan (that I’m not sure she’s aware of), was that she bought this specific pair of jeans from a store called “GU”. She seemed really excited about it. After she had gone back to the states, Kellie and I were still in Japan and had decided to stop by “GU” one more time. I ended up buying the same exact pair that she had (I now know I fit a women’s XXL pants in Japan). I’m sure she’ll love hearing about that and twinning with me in the future!
+        </p>
+
+        <h3>NHM and KHK(M)</h3>
+        <p>
+        And lastly, nothing too crazy has happened for us in 2024… I guess we got married so that’s something. But in all seriousness, it’s been a crazy busy year. We mini-mooned at an awesome spa hotel in Leavenworth, travelled to several places such as Texas, KC, California, Korea, and Japan. We’re already on apartment #2. Currently we’re living in Columbia City which has been great! It’s been nice being close to the in-laws (but not too close ya know?). We’ve settled at a new church called Seattle Upper Room. And in between on the day to day, we’re both still working from home which means we spend a loooooot of time together. It’s been a great year to say the least!
+
+        </p>
+
+        <h3>Luna</h3>
+        <p>
+        It’s sad not seeing her as often, but she seems to be doing well! Whenever you first walk into the house, she’s so excited to see you for like the first 2 minutes. Then nothing… you’re kind of just there. Godspeed Luna.
+        </p>
+        <p>
+          And with that, Merry Christmas and Happy New Year. See you in 2025!
+        </p>
+        <p style={{textAlign: 'center'}}>Love,</p>
+        <p style={{textAlign: 'center'}}>The Kumasaka/Miura family</p>
+      </div>
+      {/* <form onSubmit={submitHandler}>
+        <input placeholder='Name' id='user' onChange={(event) => setName(event.target.value)} name='user' value={name}/>
+        <input placeholder='Comment' id='comment' onChange={(event) => setComment(event.target.value)}  name='comment' value={comment}/>
+        <button type='submit'>Submit</button>
+      </form> */}
+    </>
   );
 }
 
